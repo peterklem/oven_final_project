e752657453f9719b27473d20ef002d65d29943f9
trmyint97 <56275094+trmyint97@users.noreply.github.com>
2020-08-07 15:46:15 -0400

    Added User edit Timer

--- Timer.v
+++ Timer.v
@@ -1,11 +1,31 @@
-module Timer (output reg [6:0] Z1 , input clk, output reg [6:0] Z2, output reg [6:0] Z3, output reg [6:0] Z4);
+module Timer (output reg [6:0] Z1 , input clk, output reg [6:0] Z2, output reg [6:0] Z3, output reg [6:0] Z4, input button1, input button2, input toggle_set);
 	parameter MAX_COUNT = 25000000;
 		
 	reg [28:0] count = 0;
 		
 		
 	reg new_clk;
+	
+	parameter BUTTON_LIM = 10000000; // Every less than a second
+	reg [30:0] button_count = 0;
+	reg button_clk;
+	
+
 		
+		
+	// Button timer
+	always @ (posedge clk) begin
+		if (button_count <= BUTTON_LIM) begin
+			 button_count <= button_count + 1;
+		end else begin 
+			 button_count <= 0;
+			 button_clk <= ~button_clk;
+		end
+	end
+
+
+
+	
 	always @ (posedge clk) begin
 		if (count <= MAX_COUNT) begin
 			count <= count + 1;
@@ -15,50 +35,105 @@ module Timer (output reg [6:0] Z1 , input clk, output reg [6:0] Z2, output reg [
 		end
 	end 
 	
-	reg [3:0] number = 0;
-	reg [2:0] number2 = 1;
-	reg [3:0] number3 = 0;
-	reg [2:0] number4 = 0;
+	reg [13:0] set_time = 0;
+	reg [3:0] hex0;
+	reg [2:0] hex1;
+	reg [3:0] hex2;
+	reg [2:0] hex3;
+	
+	reg[3:0] number;
+	reg[2:0] number2;
+	reg[3:0] number3;
+	reg[2:0] number4;
+	
+	reg[3:0] num;
+	reg[2:0] num2;
+	reg[3:0] num3;
+	reg[2:0] num4;
 	
 	
-	always @ (posedge new_clk) begin : countdown
-		if (number == 0 && number2 == 0 && number3 == 0 && number4 == 0) begin
-			number <= 0;
-			number2 <= 0;
-			number3 <= 0;
-			number4 <= 0;
-		end else begin
-			if (number == 0) begin
-				number <= 9;
-				number2 <= number2 - 1;
-				if (number2 == 0) begin
-					number2 <= 5;
-						number3 <= number3 - 1;
-					if (number3 == 0) begin
-						number3 <= 9;
-						number4 <= number4 - 1;
-						if (number4 == 0) begin
-							number4 <= 5;
-						end
-					end else begin
-						number3 <= number3 - 1;
-					end
-				end else begin
-					number2 <= number2 - 1;
-				end
-			end else begin
-				number <= number - 1;
+	//Setup
+	always @(posedge button_clk) begin
+		if(toggle_set == 0) begin // set Time
+			if (button1 == 0) begin
+				set_time = set_time + 5;
+				num = set_time % 10;
+				num2 = (set_time % 100) / 10;
+				num3 = (set_time / 100);
+				num4 = (set_time / 1000);
+			end else if (button2 == 0) begin
+				set_time = set_time - 5;
+				num = set_time % 10;
+				num2 = (set_time % 100) / 10;
+				num3 = (set_time / 100);
+				num4 = (set_time / 1000);
 			end
 		end
-		
-		
-			
-
-		
 	end
 	
+	//Countdown 
+	always @ (posedge new_clk) begin
+		if (toggle_set == 0) begin
+			number = num;
+			number2 = num2;
+			number3 = num3;
+			number4 = num4;
+		end else if (toggle_set == 1) begin //Start timer
+			if (number == 0 && number2 == 0 && number3 == 0 && number4 == 0) begin
+					number <= 0;
+					number2 <= 0;
+					number3 <= 0;
+					number4 <= 0;
+			end else begin
+				if (number == 0) begin
+					number <= 9;
+					number2 <= number2 - 1;
+					if (number2 == 0) begin
+						number2 <= 5;
+						number3 <= number3 - 1;
+						if (number3 == 0) begin
+							number3 <= 9;
+							number4 <= number4 - 1;
+							if (number4 == 0) begin
+								number4 <= 5;
+							end
+						end else begin
+							number3 <= number3 - 1;
+						end
+					end else begin
+						number2 <= number2 - 1;
+					end
+				end else begin
+					number <= number - 1;
+				end
+			end
+		end
+	end
+	
+	//Update hex display
+	
 	always @ (*) begin
-			case (number) 
+		if (toggle_set == 0) begin
+			hex0 = num;
+			hex1= num2;
+			hex2 = num3;
+			hex3 = num4;
+		end else begin
+			hex0 = number;
+			hex1 = number2;
+			hex2 = number3;
+			hex3 = number4;
+		end
+	end
+
+	
+	
+	
+	
+	
+	
+	always @ (*) begin
+			case (hex0) 
 				0: begin
 					Z1 = 7'b1000000;
 				end
@@ -93,7 +168,7 @@ module Timer (output reg [6:0] Z1 , input clk, output reg [6:0] Z2, output reg [
 		end
 		
 		always @ (*) begin
-			case (number2)
+			case (hex1)
 				0: begin
 					Z2 = 7'b1000000;
 				end
@@ -116,7 +191,7 @@ module Timer (output reg [6:0] Z1 , input clk, output reg [6:0] Z2, output reg [
 		end
 		
 		always @ (*) begin
-			case (number3) 
+			case (hex2) 
 				0: begin
 					Z3 = 7'b1000000;
 				end
@@ -151,7 +226,7 @@ module Timer (output reg [6:0] Z1 , input clk, output reg [6:0] Z2, output reg [
 		end
 		
 		always @ (*) begin
-			case (number4)
+			case (hex3)
 				0: begin
 					Z4 = 7'b1000000;
 				end
